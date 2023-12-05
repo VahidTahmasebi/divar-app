e3eb8348f185a135ba948baeb85e8866e28b20c5
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-05 16:33:14 +0330

    Complete product component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,20 +2,31 @@ import Image from "next/image";
 
 import { BsChat } from "react-icons/bs";
 
+import truncateText from "@/utils/truncateText";
+import { toPersianNumbersWithComma } from "@/utils/toPersianNumbers";
+
 const Product = ({ product }) => {
   const { title, distance, status, price, date, option, image_Url } = product;
 
   return (
-    <div className="flex justify-between items-center p-4 border-2 rounded-lg">
-      <div>
-        <h3 className="font-bold">{title}</h3>
-        <p>{distance}</p>
-        <p>{status}</p>
-        <p>{price}</p>
+    <div className="h-full flex justify-between gap-x-2 p-3 border-2 rounded-lg">
+      <div className="w-7/12 flex flex-col justify-between">
+        <h3 className="text-sm font-bold leading-6">
+          {truncateText(title, 42)}
+        </h3>
+        <div className="text-sm text-secondary-500">
+          <p>{toPersianNumbersWithComma(distance)}</p>
+          <p>{status}</p>
+          <p>{toPersianNumbersWithComma(price)}</p>
 
-        <div className="w-11/12 flex justify-between items-center">
-          {option ? <p className="text-red-600">{option}</p> : <p>{date}</p>}
-          <BsChat />
+          <div className="flex justify-between items-center">
+            {option ? (
+              <p className="text-primary-800">{option}</p>
+            ) : (
+              <p>{date}</p>
+            )}
+            <BsChat />
+          </div>
         </div>
       </div>
       <div>
