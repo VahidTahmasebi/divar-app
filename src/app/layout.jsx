11d59e0c5f49b6442c3d8aa09cdd7353237aa10f
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-05 13:23:32 +0330

    Add layout component

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import "./globals.css";
+import vazirFont from "@/constants/localFonts";
+
+import Providers from "./Providers";
+
+import Header from "@/components/Header";
+import SideBar from "@/components/SideBar";
+
+export const metadata = {
+  title: "Divar App",
+  description: "Divar App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body
+        suppressHydrationWarning={true}
+        className={`${vazirFont.variable} font-sans`}>
+        <Providers>
+          <div className="max-w-7xl container mx-auto px-2">
+            <Header />
+
+            <div className="flex">
+              <SideBar />
+              <div className="w-full">{children}</div>
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
