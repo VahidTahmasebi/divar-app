e2b304f2ff65011b68f920022d2dcb79b67ad0e0
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-05 16:32:03 +0330

    complete sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,21 +1,26 @@
 import Link from "next/link";
 
 import { sidebarCategory } from "@/constants/assets";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const SideBar = () => {
   return (
-    <div className="w-3/12 flex flex-col gap-y-3">
-      <h4>دسته‌ها</h4>
-      <ul className="flex flex-col space-y-8">
+    <div className="w-3/12 flex flex-col gap-y-3 text-sm text-secondary-500">
+      <h4 className="mb-4">دسته‌ها</h4>
+      <ul className="flex flex-col space-y-5">
         {sidebarCategory.map((item) => (
           <li key={item.title}>
             <Link href="/" className="flex gap-x-2">
               <span>{item.icon}</span> <span>{item.title}</span>
-              {item.new && <span className="text-red-500">(جدید)</span>}
+              {item.new && <span className="text-primary-800">(جدید)</span>}
             </Link>
           </li>
         ))}
       </ul>
+      <div className="flex items-center gap-x-2 py-3 border-y-2">
+        <MdKeyboardArrowDown />
+        <p>قیمت</p>
+      </div>
     </div>
   );
 };
