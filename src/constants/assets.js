9395e937f7403a4891d0b3243c3cb38d92dca0d5
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-05 16:32:49 +0330

    Fix bug in assets

--- src/constants/assets.js
+++ src/constants/assets.js
@@ -8,7 +8,7 @@ import { BsLamp } from "react-icons/bs";
 import { RiBrush4Line } from "react-icons/ri";
 import { FiWatch } from "react-icons/fi";
 import { MdOutlineSportsBasketball } from "react-icons/md";
-import { GoPeople } from "react-icons/go";
+import { IoPeopleOutline } from "react-icons/io5";
 import { MdOutlineChairAlt } from "react-icons/md";
 import { IoBriefcaseOutline } from "react-icons/io5";
 import { RiMapPinUserLine } from "react-icons/ri";
@@ -59,42 +59,42 @@ export const sidebarCategory = [
     icon: <TbDeviceRemote />,
     new: false,
   },
-  // {
-  //   title: "خانه و آشپزخانه",
-  //   route: "/",
-  //   icon: <BsLamp />,
-  //   new: false,
-  // },
-  // {
-  //   title: "خدمات",
-  //   route: "/",
-  //   icon: <RiBrush4Line />,
-  //   new: false,
-  // },
-  // {
-  //   title: "وسایل شخصی",
-  //   route: "/",
-  //   icon: <FiWatch />,
-  //   new: false,
-  // },
-  // {
-  //   title: "سرگرمی و فراغت",
-  //   route: "/",
-  //   icon: <MdOutlineSportsBasketball />,
-  //   new: false,
-  // },
-  // {
-  //   title: "اجتماعی",
-  //   route: "/",
-  //   icon: <GoPeople />,
-  //   new: false,
-  // },
-  // {
-  //   title: "تجهیزات و صنعتی",
-  //   route: "/",
-  //   icon: <MdOutlineChairAlt />,
-  //   new: false,
-  // },
+  {
+    title: "خانه و آشپزخانه",
+    route: "/",
+    icon: <BsLamp />,
+    new: false,
+  },
+  {
+    title: "خدمات",
+    route: "/",
+    icon: <RiBrush4Line />,
+    new: false,
+  },
+  {
+    title: "وسایل شخصی",
+    route: "/",
+    icon: <FiWatch />,
+    new: false,
+  },
+  {
+    title: "سرگرمی و فراغت",
+    route: "/",
+    icon: <MdOutlineSportsBasketball />,
+    new: false,
+  },
+  {
+    title: "اجتماعی",
+    route: "/",
+    icon: <IoPeopleOutline />,
+    new: false,
+  },
+  {
+    title: "تجهیزات و صنعتی",
+    route: "/",
+    icon: <MdOutlineChairAlt />,
+    new: false,
+  },
   {
     title: "استخدام و کاریابی",
     route: "/",
