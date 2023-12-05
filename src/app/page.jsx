f8ca36fcf46105dbeb059b9238b3e356cd8210c7
Vahid <vahid.tahmasebi95@gmail.com>
2023-12-05 13:23:09 +0330

    Add home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,25 @@
+"use client";
+
+import { useGetProducts } from "@/hooks/useProducts";
+
+import Product from "@/components/Product";
+
 const Homepage = () => {
-  return <div className="flex flex-wrap gap-y-20 mt-7">{/* products */}</div>;
+  const { isLoading, data: products } = useGetProducts();
+
+  return (
+    <div>
+      <h1 className="text-left text-xs text-secondary-500">
+        دیوار قائم‌شهر، نیازمندی های رایگان، آگهی‌های خرید
+      </h1>
+      <div className="grid grid-cols-3 grid-rows-3 gap-4 mt-7">
+        {products &&
+          products?.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
